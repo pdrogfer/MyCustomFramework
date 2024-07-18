36c821d495294638ba524d2808a21f5a61779344
Pedro Gonzalez <pdro74@hotmail.com>
2024-07-18 07:56:05 +0200

    first commit

---
+++ Sources/MyCustomFramework/CustomSDK.swift
@@ -0,0 +1,14 @@
+//
+//  CustomSDK.swift
+//  CustomFrameworkProjectApp
+//
+//  Created by Pedro Gonzalez Ferrandez on 18.07.24.
+//
+
+import Foundation
+
+public struct CustomSDK {
+    public static func doSomething() {
+        print("CustomSDK doing something...")
+    }
+}
